9fc3d6d69e4f0f45144d5c33419f5aed385c692a
Carlvin Mwange <cmwange@innova.co.ke>
2020-05-30 12:19:15 +0300

    yet again, more weird stuff

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.MathJax = {
   CommonHTML: {linebreaks: {automatic: true}},
   tex2jax: {
@@ -8,21 +7,4 @@ window.MathJax = {
   },
   TeX: {noUndefined: {attributes: {mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%'}}},
   messageStyle: 'none'
-=======
-// MathJax Configuration
-//
-// v2 to v3 upgrade notes:
-// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
-// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
-window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    processEscapes: false,
-    packages: {'[+]': ['noerrors']}
-  },
-  loader: {
-    load: ['[tex]/noerrors']
-  }
->>>>>>> aa7b99dc191d4e822dc5b10ae387008366fd6556
 };
